b04549a81c8e9e5a22d1de939d75442786420924
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2022-11-03 15:53:52 +0600

    gorm hook example

--- internal/task3/models/models.go
+++ internal/task3/models/models.go
@@ -1,10 +1,25 @@
 package models
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID        int
 	Login     string
 	Blocked   bool
 	LastLogin time.Time
+}
+
+// is not actually called if good record not found
+func (u *User) AfterFind(tx *gorm.DB) (err error) {
+	if u.Login == "" {
+		log.Printf("Found no record for user with ID = %d", u.ID)
+	} else {
+		log.Printf("Found good record for user with ID = %d", u.ID)
+	}
+	return
 }
